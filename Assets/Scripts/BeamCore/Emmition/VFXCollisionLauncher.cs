9dcadafca615bda5fc968e1bf52cf2a9547b0557
kizor4ik <kizorph.d@gmail.com>
2022-08-14 19:20:18 +0700

    Added simple collision Explosion

---
+++ Assets/Scripts/BeamCore/Emmition/VFXCollisionLauncher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFXCollisionLauncher : MonoBehaviour
+{
+    [SerializeField] private bool _isActive = false;
+    void Start()
+    {
+        if(_isActive)
+        {
+            BeamLauncher.BeamCollideEvent += ActivateEffect;
+            gameObject.SetActive(false);
+        }
+    }
+    private void OnDestroy()
+    {
+        BeamLauncher.BeamCollideEvent -= ActivateEffect;
+    }
+
+
+    private void ActivateEffect()
+    {
+        Debug.Log("VFX happens");
+        gameObject.SetActive(true);
+    }
+}
